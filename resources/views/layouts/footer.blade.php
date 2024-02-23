2731a1cc7e08c00a2058c8b04143662af8195158
StefenDustin <stevendustin130202@gmail.com>
2024-02-23 11:21:33 +0700

    login page, footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,6 @@
-<footer class="footer py-4 bg-dark text-light">
+<footer class="footer py-2  bg-dark text-light">
     <div class="container text-center">
+        <br>
         <p class="mb-4 small">Copyright <script>document.write(new Date().getFullYear())</script> v1.0 &copy; <a href="/" class="text-light">HCM PT. Niramas Utama</a></p>
     </div>
 </footer>
