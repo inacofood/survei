9fc2507039bd7c582506534d73d49ba9de82c028
StefenDustin <stevendustin130202@gmail.com>
2024-02-26 13:15:14 +0700

    perbaikan footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer py-2  bg-dark text-light">
+<footer class="footer py-2  bg-dark text-light" >
     <div class="container text-center">
         <br>
         <p class="mb-4 small">Copyright <script>document.write(new Date().getFullYear())</script> v1.0 &copy; <a href="/" class="text-light">HCM PT. Niramas Utama</a></p>
