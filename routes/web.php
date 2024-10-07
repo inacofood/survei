519b2533023f1148f9a514234c220f79f646a231
adityawelly <wellyadit@gmail.com>
2024-10-07 09:46:02 +0700

    tess

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ListController;
 use App\Http\Controllers\CcisController;
-<<<<<<< HEAD
-=======
-
-
->>>>>>> e10d5d9a037c4314d5c8a398cab2c44409d24ebf
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -44,12 +39,12 @@ Route::get('/ccis', [CcisController::class, 'index'])->name('ccis');
 Route::get('/ocai', [CcisController::class, 'index'])->name('ccis');
 Route::get('/kategori', [CcisController::class, 'indexkategori'])->name('kategori.index');
 Route::post('/kategori', [CcisController::class, 'store'])->name('kategori.store');
-Route::get('/kategori/{id}/edit', [CcisController::class, 'edit'])->name('kategori.edit'); 
+Route::get('/kategori/{id}/edit', [CcisController::class, 'edit'])->name('kategori.edit');
 Route::put('/kategori/{id}', [CcisController::class, 'update'])->name('kategori.update');
 Route::delete('/kategori/{id}', [CcisController::class, 'destroy'])->name('kategori.destroy');
 Route::get('/title', [CcisController::class, 'indextitle'])->name('title.index');
 Route::post('/title', [CcisController::class, 'storetitle'])->name('title.store');
-Route::get('/title/{id}/edit', [CcisController::class, 'edittitle'])->name('title.edit'); 
+Route::get('/title/{id}/edit', [CcisController::class, 'edittitle'])->name('title.edit');
 Route::put('/title/{id}', [CcisController::class, 'updatetitle'])->name('title.update');
 Route::delete('/title/{id}', [CcisController::class, 'destroytitle'])->name('title.destroy');
 Route::post('/ocai-store', [CcisController::class, 'ocaistore'])->name('ocai.store');
